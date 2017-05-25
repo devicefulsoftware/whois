8d7b778c0a61d7fcce7a73a5a4988a885add2867
UsrCletus <usrcletus@gmail.com>
2017-05-24 23:40:00 -0500

    First build should now properly display ads.

--- app/src/main/java/com/devicefulsoftware/whois/MainActivity.java
+++ app/src/main/java/com/devicefulsoftware/whois/MainActivity.java
@@ -3,11 +3,21 @@ package com.devicefulsoftware.whois;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.NativeExpressAdView;
+
 public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(getApplicationContext(),"ca-app-pub-6633448073266283~9823432419");
+        NativeExpressAdView adView = (NativeExpressAdView)findViewById(R.id.adView);
+        AdRequest request = new AdRequest.Builder().build();
+        adView.loadAd(request);
     }
 }
